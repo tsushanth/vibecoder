01741f49e60542a273fc468b9a35c2cab26de3f0
Sushanth Tiruvaipati <tsushanth@users.noreply.github.com>
2026-03-14 22:41:11 -0700

    Revert "Fix --set-build-env-vars flag in deploy.sh"
    This reverts commit fbe5b47137c9627a2653e7f9d9a159324e3b841f.

--- web/deploy.sh
+++ web/deploy.sh
@@ -20,7 +20,7 @@ gcloud run deploy ${SERVICE_NAME} \
   --cpu 1 \
   --min-instances 0 \
   --max-instances 40 \
-  --set-build-env-vars "NEXT_PUBLIC_SUPABASE_URL=https://owvvrljdfnhntwedepkl.supabase.co,NEXT_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93dnZybGpkZm5obnR3ZWRlcGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMDA1NTEsImV4cCI6MjA4Njc3NjU1MX0.WjjwtJn03_5Ayd2Ed9WlQ-lIWiZiTrlfnCl-7nYCoGk,NEXT_PUBLIC_API_URL=https://vibecoder-api-917362189743.us-central1.run.app" \
+  --build-env-vars "NEXT_PUBLIC_SUPABASE_URL=https://owvvrljdfnhntwedepkl.supabase.co,NEXT_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93dnZybGpkZm5obnR3ZWRlcGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMDA1NTEsImV4cCI6MjA4Njc3NjU1MX0.WjjwtJn03_5Ayd2Ed9WlQ-lIWiZiTrlfnCl-7nYCoGk,NEXT_PUBLIC_API_URL=https://vibecoder-api-917362189743.us-central1.run.app" \
   --set-env-vars "NODE_ENV=production"
 
 SERVICE_URL=$(gcloud run services describe ${SERVICE_NAME} --region ${REGION} --format='value(status.url)')
